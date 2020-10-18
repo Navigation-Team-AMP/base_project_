a30e5efa629d4c4c90419984989e8bd3e8c6f872
arefmalek <malek.aref1@gmail.com>
2020-10-18 09:34:14 -1000

    bit the bullet and made a separate folder

---
+++ drive.py
@@ -0,0 +1,81 @@
+#parsing command line arguments
+import argparse
+#decoding camera images
+import base64
+#for frametimestamp saving
+from datetime import datetime
+#reading and writing files
+import os
+#high level file operations
+import shutil
+#matrix math
+import numpy as np
+#real-time server
+import socketio
+#concurrent networking 
+import eventlet
+#web server gateway interface
+import eventlet.wsgi
+#image manipulation
+from PIL import Image
+#web framework
+from flask import Flask
+#input output
+from io import BytesIO
+# Load model
+from model import Model
+
+#initialize our server
+sio = socketio.Server()
+#our flask (web) app
+app = Flask(__name__)
+
+model = Model()
+model.loadModel()
+
+#registering event handler for the server
+@sio.on('telemetry')
+def telemetry(sid, data):
+    if data:
+        # The current steering angle of the car
+        steering_angle = float(data["steering_angle"])
+        # The current throttle of the car, how hard to push peddle
+        throttle = float(data["throttle"])
+        # The current speed of the car
+        speed = float(data["speed"])
+        # Compute steering angle of the car    
+        image = Image.open(BytesIO(base64.b64decode(data["image"])))
+        model_output = model.predict(image, preloaded=True)
+        steering_angle = model_output[0][0].item()
+        speed_target = model_output[0][1].item() * 30 # multiply by max speed since it was previously normalized
+        # Compute speed
+        # speed_target = 50 - abs(steering_angle) / 0.5 * 10
+        # throttle = 0.2 - abs(steering_angle) / 0.4 * 0.15
+        throttle = (speed_target - speed) * 0.5
+        print("network prediction -> (steering angle: {:.3f}, throttle: {:.3f})".format(steering_angle, throttle))
+
+        send_control(steering_angle, throttle)
+
+@sio.on('connect')
+def connect(sid, environ):
+    print("connect ", sid)
+    send_control(0, 0)
+
+
+def send_control(steering_angle, throttle):
+    sio.emit(
+        "steer",
+        data={
+            'steering_angle': steering_angle.__str__(),
+            'throttle': throttle.__str__()
+        },
+        skip_sid=True)
+
+
+if __name__ == '__main__':
+    
+    # wrap Flask application with engineio's middleware
+    app = socketio.Middleware(sio, app)
+
+    # deploy as an eventlet WSGI server
+    eventlet.wsgi.server(eventlet.listen(('', 4567)), app)
